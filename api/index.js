19e0772bcc6dc7c45224657523e5c811c6d29af8
cuongvu3009 <cuongvu3009@gmail.com>
2022-01-02 22:50:07 +0200

    change port var

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ app.use('/api/users', userRoute);
 app.use('/api/posts', postRoute);
 app.use('/api/categories', categoryRoute);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Backend is running on port ${PORT}`);
 });
